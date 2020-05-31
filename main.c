b9078815c8a6e5f7ccb3fffbe37c96a4b7dc0718
Ilan Fritzler <ilan_fritzler@hotmail.com>
2020-05-31 03:35:10 -0300

    Agregada funcion de Calculo de Daño

--- main.c
+++ main.c
@@ -1,41 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <Windows.h>
+#include <time.h>
 
 #include "pokemon.h"
 #include "gotoxy.h"
 #include "menu.h"
 
-#define HP 219
-#define BLACK 0
-#define GREEN 2
-#define YELLOW 14
-#define RED 4
-#define WHITE 15
-#define LGREY 7
+
 
 void inicHPBAR( char array_hp[], int dim ) ;
 void inicGREYBAR( char hp_gastados[], int dim );
 int porcientoHP( int psActual, int psTotal, int dimArreglo );
 void printHPBAR( char array_hp[], int posFinal );
 void printHPGastados( char hp_gastados[], int dim, int posArran );
-void showHPBAR(int psActual, int psTotal, char array_hp[], char hp_gastados[], int dimension_HPBAR);
-
+void showHPBAR(stPkmn archivoPokemon[], int idPokemon, int x, int y);
+int calculoDanio(stPkmn pkmAtaca, stPkmn pkmnDefiende);
 
 int main() {
-    char hp_gastados[20];
-    char ps[20];
-    inicGREYBAR( hp_gastados, 20 );
-    inicHPBAR( ps, 20 );
-    int psActual = 325;
-    int psTotal = 325;
-    do {
+    hidecursor(0);
+    srand(time(NULL));
+    FILE* archivoPokemon;
 
-        showHPBAR( psActual, psTotal, ps, hp_gastados, 20 );
-        psActual--;
-        Sleep( 25 );
-        system( "cls" );
-    } while( psActual >= 0 );
+
 
     return 0;
 }
@@ -73,20 +60,55 @@ void printHPGastados( char hp_gastados[], int dim, int posArran ) {
     }
 }
 
-void showHPBAR(int psActual, int psTotal, char array_hp[], char hp_gastados[], int dimension_HPBAR) {
+void showHPBAR(stPkmn archivoPokemon[2], int idPokemon, int x, int y) {
+    //Inicializacion de las barras
+    char hp_gastados[DIMHP];
+    char barra_ps[DIMHP];
+    inicGREYBAR( hp_gastados, DIMHP );
+    inicHPBAR( barra_ps, DIMHP );
+    //Inicializacion de los datos
     int dimHPBAR;
     int barraPS;
-
+    int psTotal = archivoPokemon[idPokemon + 1]._Est.psTOTAL;
+    int psActual = archivoPokemon[idPokemon + 1]._Est.psTOTAL;
     barraPS = porcientoHP( psActual, psTotal, 100 );
-    dimHPBAR = porcientoHP( psActual, psTotal, dimension_HPBAR );
+    dimHPBAR = porcientoHP( psActual, psTotal, DIMHP );
 
+    gotoxy(x, y);
+    printf("%s\n", archivoPokemon[idPokemon + 1].name);
     ( barraPS > 50 ) ? Color( BLACK, GREEN ) : ( barraPS > 20 ) ? Color( BLACK, YELLOW ) : Color( BLACK, RED );
-    gotoxy(20, 21);
-    printHPBAR( array_hp, dimHPBAR );
+
+    gotoxy(x, y + 1);
+    printHPBAR( barraPS, dimHPBAR );
     Color( BLACK, LGREY );
-    printHPGastados( hp_gastados, dimension_HPBAR, dimHPBAR );
+    printHPGastados( hp_gastados, dimHPBAR, DIMHP );
     Color( BLACK, WHITE );
-    //printf("\n");
-    gotoxy(20, 22);
+    gotoxy(x, y + 2);
     printf( "%3d/%3d\t\t%d%%", psActual, psTotal, barraPS );
+}
+
+int calculoDanio(stPkmn pkmAtaca, stPkmn pkmnDefiende){
+    float danio;
+    //Sistema de niveles NO IMPLEMENTADO, se presupone que todos los pokemon son de nivel 100 por el momento.
+    int nivel = 100;
+    //Daño de poder segun el ataque NO IMPLEMENTADO
+    int poder = 70;
+    //Bonificacion depende del ataque que realiza, si es del mismo tipo que el pokemon sera mas efectivo.
+    float bonificacion = 1.0;
+    //Efectividad: depende de que tan eficaz es el tipo de ataque con que se ataca y el tipo del pokemon.
+    float efectividad = 1.0;
+    //Variacion: Valor incalculable, sirve para dar aleatoriedad al daño causado.
+    float variacion = (rand() % 16 + 85);
+    //Todas las variables que intervienen en el calculo estan cargadas para posibles modificaciones futuras.
+    danio = 0.01 * bonificacion * efectividad * variacion * ( (((0.2 * nivel + 1) * pkmAtaca._Est.ATK * poder) / (25 * pkmnDefiende._Est.DEF) ) + 2 );
+
+    return (int) danio;
+}
+
+
+void cargarPokemonEnArchivo(FILE* archivo){
+
+    fopen("archivoPokemon.bin", "ab");
+
+
 }
--- pokemon.h
+++ pokemon.h
@@ -1,6 +1,20 @@
 #ifndef __POKEMON_H__
 #define __POKEMON_H__
 
+///CONSTANTES
+#define HP 219
+#define BLACK 0
+#define GREEN 2
+#define YELLOW 14
+#define RED 4
+#define WHITE 15
+#define LGREY 7
+#define HPBAR1_X 10
+#define HPBAR1_Y 21
+#define HPBAR2_X 60
+#define HPBAR2_Y 4
+#define DIMHP 20
+
 typedef struct{
     int psTOTAL;
     int ATK;
@@ -17,7 +31,7 @@ typedef struct{
 */
 typedef struct {
     int id;
-    char name[10];
+    char name[15];
     //char* types[10]; //ToDo
     stStatics _Est;
     //stAtaque ataques[4];
