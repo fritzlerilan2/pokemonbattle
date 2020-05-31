7538a95cc600088616e9034e9c53e0ee3c183278
Ilan Fritzler <ilan_fritzler@hotmail.com>
2020-05-31 00:21:33 -0300

    Cambio Nombres de funciones y variables

--- main.c
+++ main.c
@@ -14,23 +14,24 @@
 #define WHITE 15
 #define LGREY 7
 
-void inic_HPBAR( char* ps, int dim );
-void inicMascara( char* mascara, int dim );
-int calcularPorcHP( int psActual, int psTotal, int dimArreglo );
-void printHPBAR( char* HPBAR, int posFinal );
-void printHPMascara( char* mascara, int dim, int posArran );
-void HPBAR( int psActual, int psTotal, char* ps, char* mascara, int dimension );
+void inicHPBAR( char array_hp[], int dim ) ;
+void inicGREYBAR( char hp_gastados[], int dim );
+int porcientoHP( int psActual, int psTotal, int dimArreglo );
+void printHPBAR( char array_hp[], int posFinal );
+void printHPGastados( char hp_gastados[], int dim, int posArran );
+void showHPBAR(int psActual, int psTotal, char array_hp[], char hp_gastados[], int dimension_HPBAR);
 
 
 int main() {
-    char mascara[20];
+    char hp_gastados[20];
     char ps[20];
-    inicMascara( mascara, 20 );
-    inic_HPBAR( ps, 20 );
+    inicGREYBAR( hp_gastados, 20 );
+    inicHPBAR( ps, 20 );
     int psActual = 325;
     int psTotal = 325;
     do {
-        HPBAR( psActual, psTotal, ps, mascara, 20 );
+
+        showHPBAR( psActual, psTotal, ps, hp_gastados, 20 );
         psActual--;
         Sleep( 25 );
         system( "cls" );
@@ -43,45 +44,49 @@ int main() {
 
 
 
-void inicMascara( char* mascara, int dim ) {
+void inicGREYBAR( char hp_gastados[], int dim ) {
     for( int i = 0; i < dim; i++ ) {
-        mascara[i] = HP;
+        hp_gastados[i] = HP;
     }
 }
 
-void inic_HPBAR( char* ps, int dim ) {
+void inicHPBAR( char array_hp[], int dim ) {
     for( int i = 0; i < dim; i++ ) {
-        ps[i] = HP;
+        array_hp[i] = HP;
     }
 }
 
-int calcularPorcHP( int psActual, int psTotal, int dimArreglo ) {
+int porcientoHP( int psActual, int psTotal, int dimArreglo ) {
     int parteEntera = ( psActual * dimArreglo ) / psTotal;
     return parteEntera;
 }
 
-void printHPBAR( char* HPBAR, int posFinal ) {
+void printHPBAR( char array_hp[], int posFinal ) {
     for( int i = 0; i < posFinal; i++ ) {
-        printf( "%c", HPBAR[i] );
+        printf( "%c", array_hp[i] );
     }
 }
 
-void printHPMascara( char* mascara, int dim, int posArran ) {
+void printHPGastados( char hp_gastados[], int dim, int posArran ) {
     for( int i = posArran; i < dim; i++ ) {
-        printf( "%c", mascara[i] );
+        printf( "%c", hp_gastados[i] );
     }
 }
 
-void HPBAR(int psActual, int psTotal, char* ps, char* mascara, int dimension) {
+void showHPBAR(int psActual, int psTotal, char array_hp[], char hp_gastados[], int dimension_HPBAR) {
     int dimHPBAR;
-    int porcentaje;
-    porcentaje = calcularPorcHP( psActual, psTotal, 100 );
-    dimHPBAR = calcularPorcHP( psActual, psTotal, dimension );
-    ( porcentaje > 50 ) ? Color( BLACK, GREEN ) : ( porcentaje > 20 ) ? Color( BLACK, YELLOW ) : Color( BLACK, RED );
-    printHPBAR( ps, dimHPBAR );
+    int barraPS;
+
+    barraPS = porcientoHP( psActual, psTotal, 100 );
+    dimHPBAR = porcientoHP( psActual, psTotal, dimension_HPBAR );
+
+    ( barraPS > 50 ) ? Color( BLACK, GREEN ) : ( barraPS > 20 ) ? Color( BLACK, YELLOW ) : Color( BLACK, RED );
+    gotoxy(20, 21);
+    printHPBAR( array_hp, dimHPBAR );
     Color( BLACK, LGREY );
-    printHPMascara( mascara, dimension, dimHPBAR );
+    printHPGastados( hp_gastados, dimension_HPBAR, dimHPBAR );
     Color( BLACK, WHITE );
-    printf("\n");
-    printf( "%4d/%4d    %d%%", psActual, psTotal, porcentaje );
+    //printf("\n");
+    gotoxy(20, 22);
+    printf( "%3d/%3d\t\t%d%%", psActual, psTotal, barraPS );
 }
